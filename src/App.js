257815e0ac82c601e1972b2df28778a4fc8016ba
Кирилл <kolosovkirill90@gmail.com>
2018-12-10 23:31:29 +0200

    Added modal window for new contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import Modal from "./components/modal";
 
 class App extends Component {
+  state = {
+    showModal: false
+  };
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+    console.log(this.state.showModal);
+  };
+
   render() {
     return (
       <div className="App">
-        <button className="addContactModal">Add new contact</button>
+        <button className="addContactModal" onClick={this.toggleModal}>
+          Add new contact
+        </button>
+        <Modal
+          toggleModal={this.toggleModal}
+          showModal={this.state.showModal}
+        />
       </div>
     );
   }
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,13 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Modal extends Component {
-  render() {
-    return (
-      <div className="Modal">
-        <p>Your phone book is empty</p>
-      </div>
-    );
-  }
-}
+export const Modal = ({
+  inputPhone,
+  inputName,
+  inputCompany,
+  inputImage,
+  showModal,
+  toggleModal
+}) => (
+  <div className={`createContact ${showModal ? "open" : ""}`}>
+    <div className="wrapper">
+      <button className="closeModal" onClick={toggleModal}>
+        +
+      </button>
+      <h2>Add new contact</h2>
+      <input
+        type="tel"
+        name="inputPhone"
+        placeholder="Phone number"
+        value={inputPhone}
+      />
+      <input
+        type="text"
+        name="inputName"
+        placeholder="Name"
+        value={inputName}
+      />
+      <input
+        type="text"
+        name="inputCompany"
+        placeholder="Company"
+        value={inputCompany}
+      />
+      <input
+        type="text"
+        name="inputImage"
+        placeholder="Url for photo"
+        value={inputImage}
+      />
+      <button>Add to phone book</button>
+    </div>
+  </div>
+);
 
 export default Modal;
